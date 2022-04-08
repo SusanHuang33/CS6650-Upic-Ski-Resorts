565388a3bedae5c0bd386835f272908878486904
Shan Huang <huang.shan1@northeastern.edu>
2022-04-08 13:27:58 -0700

    Server for A3

---
+++ A3/serverDB/src/main/java/model/Season.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Season {
+    private String year;
+
+    public Season(String year) {
+        this.year = year;
+    }
+
+    public String getYear() {
+        return year;
+    }
+}
